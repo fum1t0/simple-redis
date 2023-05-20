44eb05045208ce3766ced3e0f7186653280249db
fum1t0 <fumi1t0.eng@gmail.com>
2023-05-20 18:29:16 +0900

    handle multiple connections

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from socket import create_server, socket
+from threading import Thread
+
+
+def main() -> None:
+    server_socket: socket = create_server(("localhost", 6379), reuse_port=True)
+
+    while True:
+        client_connection, _ = server_socket.accept()  # wait for client
+        Thread(target=handle_connection, args=(client_connection,)).start()
+
+
+def handle_connection(client_connection: socket) -> None:
+    while True:
+        try:
+            client_connection.recv(1024)
+            client_connection.send(b"+PONG\r\n")
+        except ConnectionError:
+            break
+
+
+if __name__ == "__main__":
+    main()
